7ff3abc699b0c9cecdedde58e515856e42e6c716
Adam Zielinski <adam.zie231@gmail.com>
2024-03-14 12:50:42 +0100

    thank you page button

--- src/Pages/ThankYou.jsx
+++ src/Pages/ThankYou.jsx
@@ -1,10 +1,16 @@
+import { redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import classes from "./ThankYou.module.css";
 
 export default function ThankYouPage() {
     return (
         <main className={classes.container}>
             <h2>Thank you</h2>
-            <p>That's all! Thank you for using our service</p>
+            <p>
+                That&apos;s all! Thank you for using our service. You can now
+                close this site or visit us
+            </p>
+            <Link to="/">Check us</Link>
         </main>
     );
 }

de1908d05df9b91cc4b25841a1a2b545cd1a87c8
Patryk Urbanowicz <patryk.urbanowicz.code@gmail.com>
2024-04-18 13:17:08 +0200

    Added styling to forms and introduced css modules.

--- src/components/auth/RegisterForm.jsx
+++ src/components/auth/RegisterForm.jsx
@@ -1,63 +1,70 @@
 import { Form } from "react-router-dom";
-import "./RegisterForm.css";
+import classes from "./RegisterForm.module.css";
 
 export default function RegisterForm() {
     return (
         <div className={"rightSideRegister"}>
-            <Form method={"post"} className="registerForm">
-                <h1>Rejestracja</h1>
-                <p>
-                    <label htmlFor="name">Imię</label>
-                    <input id="name" name="name" required />
-                </p>
-                <p>
-                    <label htmlFor="surname">Nazwisko</label>
-                    <input id="surname" name="surname" required />
-                </p>
-                <p>
-                    <label htmlFor="mail">E-mail</label>
-                    <input id="mail" type="email" name="mail" required />
-                </p>
-                <p>
-                    <label htmlFor="password">Hasło</label>
-                    <input
-                        id="password"
-                        type="password"
-                        name="password"
-                        required
-                    />
-                </p>
-                <p>
-                    <label htmlFor="mail">Powtórz Hasło</label>
-                    <input
-                        id="retypedPassword"
-                        type="password"
-                        name="retypedPassword"
-                        required
-                    />
-                </p>
-                <p>
-                    <label htmlFor="location">Lokacja</label>
-                    <input id="location" name="location" required />
-                </p>
-                <p>
-                    <label htmlFor="bankNumber">Numer konta</label>
-                    <input
-                        id="bankAccountNumber"
-                        name="bankAccountNumber"
-                        minLength="26"
-                        maxLength="26"
-                        pattern="^[0-9]{26}$"
-                        onInvalid={(e) =>
-                            e.target.setCustomValidity(
-                                "Niepoprawny numer konta"
-                            )
-                        }
-                        onInput={(e) => e.target.setCustomValidity("")}
-                        required
-                    />
-                </p>
-                <button>Zarejestruj się</button>
+            <Form method={"post"} className={classes.registerForm}>
+                <img
+                    src="/register.png"
+                    alt={"Zapraszamy do rejestacji!"}
+                    className={classes.registerImage}
+                />
+                <div className={classes.formArea}>
+                    <h1>Rejestracja</h1>
+                    <p>
+                        <label htmlFor="name">Imię</label>
+                        <input id="name" name="name" required />
+                    </p>
+                    <p>
+                        <label htmlFor="surname">Nazwisko</label>
+                        <input id="surname" name="surname" required />
+                    </p>
+                    <p>
+                        <label htmlFor="mail">E-mail</label>
+                        <input id="mail" type="email" name="mail" required />
+                    </p>
+                    <p>
+                        <label htmlFor="password">Hasło</label>
+                        <input
+                            id="password"
+                            type="password"
+                            name="password"
+                            required
+                        />
+                    </p>
+                    <p>
+                        <label htmlFor="mail">Powtórz Hasło</label>
+                        <input
+                            id="retypedPassword"
+                            type="password"
+                            name="retypedPassword"
+                            required
+                        />
+                    </p>
+                    <p>
+                        <label htmlFor="location">Lokacja</label>
+                        <input id="location" name="location" required />
+                    </p>
+                    <p>
+                        <label htmlFor="bankNumber">Numer konta</label>
+                        <input
+                            id="bankAccountNumber"
+                            name="bankAccountNumber"
+                            minLength="26"
+                            maxLength="26"
+                            pattern="^[0-9]{26}$"
+                            onInvalid={(e) =>
+                                e.target.setCustomValidity(
+                                    "Niepoprawny numer konta"
+                                )
+                            }
+                            onInput={(e) => e.target.setCustomValidity("")}
+                            required
+                        />
+                    </p>
+                    <button>Zarejestruj się</button>
+                </div>
             </Form>
         </div>
     );

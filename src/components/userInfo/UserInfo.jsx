6225530e780beb91a8789f8833c90ec141bea375
Adam Zielinski <adam.zie231@gmail.com>
2024-04-20 21:00:50 +0200

    password change validation

--- src/components/userInfo/UserInfo.jsx
+++ src/components/userInfo/UserInfo.jsx
@@ -7,19 +7,36 @@ import { DarkModeContext } from "../DarkModeProvider";
 
 export default function UserInfo({ info }) {
     const [darkMode, setDarkMode] = useContext(DarkModeContext);
+    const [enteredPassword, setEnteredPassword] = useState({
+        oldPassword: "",
+        password: "",
+        retypedPassword: "",
+    });
+    const validPasswordElements = {
+        length:
+            enteredPassword.password.length >= 8 &&
+            enteredPassword.password.length <= 30,
+        lower: /[a-z]/.test(enteredPassword.password),
+        upper: /[A-Z]/.test(enteredPassword.password),
+        number: /[0-9]/.test(enteredPassword.password),
+        special: /[!@#$%^&*]/.test(enteredPassword.password),
+        match: enteredPassword.password === enteredPassword.retypedPassword,
+        different: enteredPassword.password !== enteredPassword.oldPassword,
+    };
+    const validPassword = Object.values(validPasswordElements).every(
+        (value) => value === true
+    );
+    const [errorMessage, setErrorMessage] = useState(false);
     const nameRef = useRef();
     const surnameRef = useRef();
     const locationRef = useRef();
-    const oldPwdRef = useRef();
-    const newPwdRef = useRef();
-    const new2PwdRef = useRef();
     const [changedData, setChangedData] = useState(false);
 
     const inputClass = `${classes.editInput} ${
         darkMode ? classes.editInputDark : ""
     }`;
 
-    async function sendData(sendPackage, endpoint) {
+    async function sendData(sendPackage, endpoint, type) {
         const token = getAuthToken();
         const fetchUrl = getBackendUrl() + endpoint;
 
@@ -32,6 +49,10 @@ export default function UserInfo({ info }) {
             },
         });
 
+        if (type === "password" && response.status === 400) {
+            console.log("error 400");
+            setErrorMessage("Błędne hasło");
+        }
         if (!response.ok) {
             throw new Error("Failed to edit data");
         }
@@ -45,36 +66,44 @@ export default function UserInfo({ info }) {
         );
     }
 
+    function handleInputChange(identifier, event) {
+        setEnteredPassword((prev) => ({
+            ...prev,
+            [identifier]: event.target.value,
+        }));
+    }
+
     function editData(event) {
         event.preventDefault();
 
-        if (changedData) {
-            const sendPackage = {
-                name: nameRef.current.value,
-                surname: surnameRef.current.value,
-                location: locationRef.current.value,
-            };
-            console.log("sendPackage");
-            console.log(sendPackage);
+        const sendPackage = {
+            name: nameRef.current.value,
+            surname: surnameRef.current.value,
+            location: locationRef.current.value,
+        };
+        console.log("sendPackage");
+        console.log(sendPackage);
 
-            sendData(sendPackage, "/user/editInformations");
-        }
+        sendData(sendPackage, "/user/editInformations");
     }
 
     function editPassword(event) {
         event.preventDefault();
 
-        const sendPackage = {
-            password: newPwdRef.current.value,
-            retypedPassword: new2PwdRef.current.value,
-        };
-        console.log("sendPackage");
-        console.log(sendPackage);
+        console.log("enteredPassword");
+        console.log(enteredPassword);
 
-        sendData(sendPackage, "/user/editPassword");
+        sendData(enteredPassword, "/user/editPassword", "password");
+    }
+    function clearPassword() {
+        setEnteredPassword({
+            oldPassword: "",
+            password: "",
+            retypedPassword: "",
+        });
+        setErrorMessage(false);
     }
 
-    console.log(info);
     return (
         <section className={classes.container}>
             <div className={classes.data}>
@@ -108,6 +137,7 @@ export default function UserInfo({ info }) {
                         <p>{info.bankAccountNumber}</p>
                     </div>
                     <button
+                        disabled={!changedData}
                         className={`${classes.button} ${
                             !changedData && classes.grayButton
                         }`}
@@ -118,23 +148,125 @@ export default function UserInfo({ info }) {
             </div>
             <div className={classes.password}>
                 <h2>Zmień hasło</h2>
+                {errorMessage && (
+                    <div className={classes.errorMessage}>{errorMessage}</div>
+                )}
                 <form className={classes.edit} onSubmit={editPassword}>
                     <input
-                        ref={oldPwdRef}
+                        type="password"
+                        name="oldPassword"
                         placeholder="stare hasło"
                         className={inputClass}
+                        onChange={(event) =>
+                            handleInputChange("oldPassword", event)
+                        }
+                        value={enteredPassword.oldPassword}
                     />
                     <input
-                        ref={newPwdRef}
+                        type="password"
+                        name="password"
                         placeholder="nowe hasło"
                         className={inputClass}
+                        onChange={(event) =>
+                            handleInputChange("password", event)
+                        }
+                        value={enteredPassword.password}
                     />
                     <input
-                        ref={new2PwdRef}
+                        type="password"
+                        name="retypedPassword"
                         placeholder="powtórz nowe hasło"
                         className={inputClass}
+                        onChange={(event) =>
+                            handleInputChange("retypedPassword", event)
+                        }
+                        value={enteredPassword.retypedPassword}
                     />
-                    <button className={classes.button}>Zmień hasło</button>
+                    <div className={classes.validationButtonsContainer}>
+                        <section className={classes.passwordValid}>
+                            <p
+                                className={
+                                    validPasswordElements.length
+                                        ? classes.validElement
+                                        : undefined
+                                }
+                            >
+                                długość (8-30)
+                            </p>
+                            <p
+                                className={
+                                    validPasswordElements.lower
+                                        ? classes.validElement
+                                        : undefined
+                                }
+                            >
+                                mała litera
+                            </p>
+                            <p
+                                className={
+                                    validPasswordElements.upper
+                                        ? classes.validElement
+                                        : undefined
+                                }
+                            >
+                                wielka litera
+                            </p>
+                            <p
+                                className={
+                                    validPasswordElements.number
+                                        ? classes.validElement
+                                        : undefined
+                                }
+                            >
+                                cyfra
+                            </p>
+                            <p
+                                className={
+                                    validPasswordElements.special
+                                        ? classes.validElement
+                                        : undefined
+                                }
+                            >
+                                znak specjalny
+                            </p>
+                            <p
+                                className={
+                                    validPasswordElements.match
+                                        ? classes.validElement
+                                        : undefined
+                                }
+                            >
+                                hasła pasują
+                            </p>
+                            <p
+                                className={
+                                    validPasswordElements.different
+                                        ? classes.validElement
+                                        : undefined
+                                }
+                            >
+                                nowe hasło jest inne
+                            </p>
+                        </section>
+                        <div className={classes.passwordButtons}>
+                            <button
+                                type="reset"
+                                className={classes.buttonReset}
+                                onClick={clearPassword}
+                            >
+                                wyczyść
+                            </button>
+                            <button
+                                disabled={!validPassword}
+                                type="submit"
+                                className={`${classes.button} ${
+                                    !validPassword && classes.grayButton
+                                }`}
+                            >
+                                Zmień hasło
+                            </button>
+                        </div>
+                    </div>
                 </form>
             </div>
             <div className={classes.qr}>

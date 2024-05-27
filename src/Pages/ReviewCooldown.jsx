17dbc4af52a0df84b5d72dfb540baa71f2a11e53
Nekio21 <108297802+Nekio21@users.noreply.github.com>
2024-05-27 16:36:40 +0200

    cooldown css

--- src/Pages/ReviewCooldown.jsx
+++ src/Pages/ReviewCooldown.jsx
@@ -1,3 +1,9 @@
+import classes from "./ReviewCooldown.module.css";
+
 export default function ReviewCooldownPage() {
-    return <div>Za dużo opinii na raz!</div>;
+    return (
+        <div className={classes.cointainerCooldown}>
+            <div className={classes.tooMuch}>Za dużo opinii na raz!</div>
+        </div>
+    );
 }

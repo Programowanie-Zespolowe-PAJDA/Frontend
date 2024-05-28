82f4b60a4e21f6e86b600af0387dc89bc4143d0e
Adam Zieliński <adam.zie231@gmail.com>
2024-05-28 22:21:09 +0200

    local false

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ import RegisterPage from "./Pages/RegisterPage.jsx";
 import { DarkModeProvider } from "./components/DarkModeProvider.jsx";
 import ReviewCooldownPage from "./Pages/ReviewCooldown.jsx";
 
-export const LOCAL = true;
+export const LOCAL = false;
 
 const router = createBrowserRouter([
     {
